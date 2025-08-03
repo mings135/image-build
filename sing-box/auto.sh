9554e3544fca08f9818a047a12c497970129950f
MingQ <mings135@163.com>
2025-08-03 09:14:56 +0800

    opt

--- sing-box/auto.sh
+++ sing-box/auto.sh
@@ -1,18 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
+# 常量
 TAG_NAME=sing-box
 REMOTE_TAGS_FILE=sing-box.json
 REMOTE_TAGS_URL=https://api.github.com/repos/SagerNet/sing-box/tags
 LOCAL_TAGS_FILE=image-build.json
 LOCAL_TAGS_URL=https://api.github.com/repos/mings135/image-build/tags
 
+# 新 tag 默认 none
+new_release_tag="none"
+
+# 安装 yq
+sudo -E curl -fsSL -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && sudo -E chmod +x /usr/local/bin/yq
+
 # 获取本地 tags
 curl -fsSL ${LOCAL_TAGS_URL} > ${LOCAL_TAGS_FILE}
 yq -rioj '.[].name' ${LOCAL_TAGS_FILE}
 local_head_ver=$(head -1 ${LOCAL_TAGS_FILE} | awk -F '/' '{print $2}')
-# 设置默认值
-tags_all="none"
 
 # 循环遍历 page, per_page 默认 100
 for i in $(seq 1 3)
@@ -22,7 +27,7 @@ do
     # 获取远程 tags
     curl -fsSL "${REMOTE_TAGS_URL}?per_page=100&page=$i" > ${REMOTE_TAGS_FILE}
     yq -rioj '.[].name' ${REMOTE_TAGS_FILE}
-    # 远程 tags 为空，跳出
+    # 远程 tags 为空，跳出循环
     if [ ! -s ${REMOTE_TAGS_FILE} ]; then
         echo "没有更多的远程 tags"
         break
@@ -38,18 +43,18 @@ do
         # 找到不存在本地的最新 tag(正式版)
         if echo "$line" | grep -Eqi 'v([[:digit:]]*\.){2}[[:digit:]]*$'; then
             if ! grep -Eqi "${line}$" ${LOCAL_TAGS_FILE}; then 
-                tags_all="${TAG_NAME}/${line}"
-                echo "创建新版本 ${tags_all}"
+                new_release_tag="${TAG_NAME}/${line}"
+                echo "创建新版本 ${new_release_tag}"
                 break 2
             fi
         fi
     done < ${REMOTE_TAGS_FILE}
 done
 
-if [ "${tags_all}" = "none" ]; then
+if [ "${new_release_tag}" = "none" ]; then
     echo "release_status=no" >> $GITHUB_OUTPUT
     echo "没有新版本"
 else
     echo "release_status=yes" >> $GITHUB_OUTPUT
-    echo "release_tag=${tags_all}" >> $GITHUB_OUTPUT
+    echo "release_tag=${new_release_tag}" >> $GITHUB_OUTPUT
 fi
