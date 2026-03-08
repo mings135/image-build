0c87497c2673d77414e4167148990f35650978ed
MingQ <mings135@163.com>
2026-03-08 11:11:53 +0800

    fix and cerbot in container

--- nginx-proxy/entrypoint.sh
+++ nginx-proxy/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # PROXY2=app,app.mqgo.top,2.2.2.2:443
 
 # CERT_SOURCE=sing-box volume(default): ./certs:/root/.local/share/certmagic(sing-box) and ./certs:/etc/nginx/certs(nginx-proxy)
-# CERT_SOURCE=certbot volume: /etc/letsencrypt:/etc/nginx/certs(nginx-proxy) and install certbot
+# CERT_SOURCE=certbot(Need EMAIL) volume: ./certs:/etc/letsencrypt
 
 
 variable_by_const() {
@@ -14,8 +14,10 @@ variable_by_const() {
     CERTS_DIR="${CONFIG_DIR}/certs"
     HTTP_DIR="${CONFIG_DIR}/conf.d"
     STREAM_DIR="${CONFIG_DIR}/stream.d"
-    CRONTAB_FILE="/etc/periodic/daily/check.sh"
+    NGINX_CRONTAB="/etc/periodic/daily/nginx.sh"
     RECORD_FILE="/tmp/record.log"
+    CERTBOT_DIR="/etc/letsencrypt"
+    CERTBOT_CRONTAB="/etc/periodic/weekly/certbot.sh"
 }
 
 variable_by_env() {
@@ -42,6 +44,10 @@ variable_by_auto() {
 check_and_init() {
     if [ ! "${PROXY1}" ]; then
         echo "ERROR: PROXY1 not config!"
+        exit 1
+    fi
+    if [ "${CERT_SOURCE}" = 'certbot' ] && [ ! "${EMAIL}" ]; then
+        echo "ERROR: EMAIL not config!"
         exit 1
     fi
     rm -rf ${HTTP_DIR} && mkdir -p ${HTTP_DIR}
@@ -118,6 +124,9 @@ nginx_http_config() {
     local tmp_domain="$(echo "$1" | awk -F ',' '{print $2}')"
     local tmp_dest="$(echo "$1" | awk -F ',' '{print $3}')"
     local tmp_port=$((10000 + $2))
+    # 依赖上面 tmp_domain 变量
+    local tmp_crt=$(eval echo "${CERT_CRT_FILE}")
+    local tmp_key=$(eval echo "${CERT_KEY_FILE}")
 
     cat >${HTTP_DIR}/${tmp_domain}.conf <<EOF
 upstream ${tmp_domain} {
@@ -131,8 +140,8 @@ server {
     
     ssl_session_timeout 5m;
     ssl_session_cache shared:SSL:50m;
-    ssl_certificate $(eval echo "${CERT_CRT_FILE}");
-    ssl_certificate_key $(eval echo "${CERT_KEY_FILE}");
+    ssl_certificate ${tmp_crt};
+    ssl_certificate_key ${tmp_key};
 
     location / {
         proxy_pass ${tmp_protocol}://${tmp_domain};
@@ -147,6 +156,10 @@ upstream web-${tmp_port} {
 EOF
 
     sed -i "/ssl_preread_server_name/a \        ${tmp_domain}  web-${tmp_port};" ${CONFIG_FILE}
+    # certbot 自动申请证书
+    if [ "${CERT_SOURCE}" = "certbot" ] && [ ! -e "${tmp_crt}" ]; then
+        certbot certonly -m ${EMAIL} --agree-tos --standalone -d ${tmp_domain}
+    fi
 }
 
 nginx_stream_config() {
@@ -193,14 +206,14 @@ nginx_config_opt() {
     sed -i "s/client_max_body_size .*/client_max_body_size ${NGINX_client_max_body_size};/" ${CONFIG_FILE}
 }
 
-nginx_check_script() {
-    cat >${CRONTAB_FILE} <<EOF
+nginx_crontab_script() {
+    cat >${NGINX_CRONTAB} <<EOF
 #!/bin/sh
 
 IS_RELOAD=0
 IS_INIT=0
 if [ ! -e ${RECORD_FILE} ]; then
-    echo '$(date +"%Y/%m/%d %H:%M"): script init' > ${RECORD_FILE}
+    echo "\$(date +"%Y/%m/%d %H:%M"): script init" > ${RECORD_FILE}
     IS_INIT=1
 fi
 for i in \$(grep '${CERTS_DIR}.*crt;' ${HTTP_DIR}/*.conf | awk '{print \$NF}' | sed 's/;//')
@@ -214,11 +227,20 @@ done
 
 if [ \${IS_RELOAD} -eq 1 ] && [ \${IS_INIT} -eq 0 ]; then
     nginx -s reload
-    echo "$(date +"%Y/%m/%d %H:%M"): reload nginx" >> ${RECORD_FILE}
+    echo "\$(date +"%Y/%m/%d %H:%M"): reload nginx" >> ${RECORD_FILE}
 fi
 EOF
-    chmod 755 ${CRONTAB_FILE}
-    ${CRONTAB_FILE}
+    chmod 755 ${NGINX_CRONTAB}
+    ${NGINX_CRONTAB}
+}
+
+certbot_crontab_script() {
+    cat >${CERTBOT_CRONTAB} <<EOF
+#!/bin/sh
+
+certbot renew --deploy-hook "nginx -s reload && date >> /tmp/certbot-renew.log"
+EOF
+    chmod 755 ${CERTBOT_CRONTAB}
 }
 
 main() {
@@ -229,7 +251,10 @@ main() {
     nginx_basic_config
     nginx_proxy_config
     nginx_config_opt
-    nginx_check_script
+    nginx_crontab_script
+    if [ "${CERT_SOURCE}" = 'certbot' ]; then
+        certbot_crontab_script
+    fi
 }
 
 main
