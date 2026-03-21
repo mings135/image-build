aa51f1b503af7542ed809d39ee2bfb7ce741a532
MingQ <mings135@163.com>
2026-03-21 20:40:44 +0800

    test6

--- sing-box/auto.sh
+++ sing-box/auto.sh
@@ -18,7 +18,7 @@ DIFF=$((NOW_TIME - RELEASE_TIME))
 
 if [ $DIFF -le 8640000 ]; then
   TAG_NAME=$(gh release list --repo $APP_REPO --exclude-pre-releases --limit 1 --json tagName --jq '.[0].tagName')
-  if gh release view "$TAG_NAME" >/dev/null 2>&1; then
+  if gh release view "$APP_NAME/$TAG_NAME" >/dev/null 2>&1; then
     echo "版本已存在: $TAG_NAME"
   else
     echo "发现新版本: $TAG_NAME"
